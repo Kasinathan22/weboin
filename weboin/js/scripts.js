803afbf069f07593272b762fd69f873632a7330c
Kasinathan22 <89632353+Kasinathan22@users.noreply.github.com>
2024-07-17 23:57:01 +0530

    Add files via upload

---
+++ weboin/js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('joinButton').addEventListener('click', function() {
+    
+    document.getElementById('form').style.display = 'none';
+    document.getElementById('btn').style.display = 'none';
+    document.getElementById('sectionHead').style.display = 'none';
+    document.getElementById('thank-you-message').style.display='block';
+});
